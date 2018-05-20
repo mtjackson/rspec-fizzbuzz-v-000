5c683e09ba4d6fcf0f7e2eb2bec69f59dddc0b1f
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-20 02:56:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 def fizzbuzz(number)
   divisible_by_three_test = number / 3
   divisible_by_five_test = number / 5
-
   if divisible_by_three_test.is_a? Integer
     return "Fizz"
   elsif divisible_by_five_test.is_a? Integer
